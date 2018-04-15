29a5a440879d6f06386c39987b4fbcfd487c91be
necky0 <baranekbbm@gmail.com>
2018-04-16 01:16:23 +0200

    last commit

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ def target_output(x):
     return np.sin(2 * np.pi * x)
 
 
-def plot_model(subplot, x_train, y_train, x, y_obj, y_model, x_val=None, y_val=None, train_err=None, val_err=None):
+def plot_model(subplot, x_train, y_train, x, y_obj, y_model, x_val=None, y_val=None, train_err=None,
+               val_err=None):
     x_min = np.min([np.min(x_train), np.min(x)])
     x_max = np.max([np.max(x_train), np.max(x)])
     y_min = -1.5
