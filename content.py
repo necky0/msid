a494b90382fb116e2e836ce098c1723c7c7ee2dc
unknown <baranekbbm@gmail.com>
2018-04-16 00:51:28 +0200

    Initial commit

---
+++ content.py
@@ -0,0 +1,117 @@
+# --------------------------------------------------------------------------
+# ------------  Metody Systemowe i Decyzyjne w Informatyce  ----------------
+# --------------------------------------------------------------------------
+#  Zadanie 1: Regresja liniowa
+#  autorzy: A. Gonczarek, J. Kaczmar, S. Zareba
+#  2017
+# --------------------------------------------------------------------------
+
+import numpy as np
+from utils import polynomial
+
+
+def mean_squared_error(x, y, w):
+    '''
+    :param x: ciag wejsciowy Nx1
+    :param y: ciag wyjsciowy Nx1
+    :param w: parametry modelu (M+1)x1
+    :return: blad sredniokwadratowy pomiedzy wyjsciami y
+    oraz wyjsciami uzyskanymi z wielowamiu o parametrach w dla wejsc x
+    '''
+
+    return np.mean((y - polynomial(x, w))**2)
+
+
+def design_matrix(x_train, M):
+    '''
+    :param x_train: ciag treningowy Nx1
+    :param M: stopien wielomianu 0,1,2,...
+    :return: funkcja wylicza Design Matrix Nx(M+1) dla wielomianu rzedu M
+    '''
+
+    return np.power(x_train, np.arange(0, M+1))
+
+
+def least_squares(x_train, y_train, M):
+    '''
+    :param x_train: ciag treningowy wejscia Nx1
+    :param y_train: ciag treningowy wyjscia Nx1
+    :param M: rzad wielomianu
+    :return: funkcja zwraca krotke (w,err), gdzie w sa parametrami dopasowanego wielomianu, a err blad sredniokwadratowy
+    dopasowania
+    '''
+
+    pattern = design_matrix(x_train, M)
+    w = np.linalg.inv(pattern.T@pattern)@pattern.T@y_train
+    err = mean_squared_error(x_train, y_train, w)
+
+    return w, err
+
+
+def regularized_least_squares(x_train, y_train, M, regularization_lambda):
+    '''
+    :param x_train: ciag treningowy wejscia Nx1
+    :param y_train: ciag treningowy wyjscia Nx1
+    :param M: rzad wielomianu
+    :param regularization_lambda: parametr regularyzacji
+    :return: funkcja zwraca krotke (w,err), gdzie w sa parametrami dopasowanego wielomianu zgodnie z kryterium z regularyzacja l2,
+    a err blad sredniokwadratowy dopasowania
+    '''
+
+    pattern = design_matrix(x_train, M)
+    w = np.linalg.inv(pattern.T@pattern + regularization_lambda*np.eye(pattern.shape[1]))@pattern.T@y_train
+    err = mean_squared_error(x_train, y_train, w)
+
+    return w, err
+
+
+def model_selection(x_train, y_train, x_val, y_val, M_values):
+    '''
+    :param x_train: ciag treningowy wejscia Nx1
+    :param y_train: ciag treningowy wyjscia Nx1
+    :param x_val: ciag walidacyjny wejscia Nx1
+    :param y_val: ciag walidacyjny wyjscia Nx1
+    :param M_values: tablica stopni wielomianu, ktore maja byc sprawdzone
+    :return: funkcja zwraca krotke (w,train_err,val_err), gdzie w sa parametrami modelu, ktory najlepiej generalizuje dane,
+    tj. daje najmniejszy blad na ciagu walidacyjnym, train_err i val_err to bledy na sredniokwadratowe na ciagach treningowym
+    i walidacyjnym
+    '''
+
+    val = []
+    for m in M_values:
+        w, _ = least_squares(x_train, y_train, m)
+        err = mean_squared_error(x_val, y_val, w)
+        val.append((w, err))
+
+    w, val_err = min(val, key=lambda e: e[1])
+
+    train_err = mean_squared_error(x_train, y_train, w)
+
+    return w, train_err, val_err
+
+
+def regularized_model_selection(x_train, y_train, x_val, y_val, M, lambda_values):
+    '''
+    :param x_train: ciag treningowy wejscia Nx1
+    :param y_train: ciag treningowy wyjscia Nx1
+    :param x_val: ciag walidacyjny wejscia Nx1
+    :param y_val: ciag walidacyjny wyjscia Nx1
+    :param M: stopien wielomianu
+    :param lambda_values: lista ze wartosciami roznych parametrow regularyzacji
+    :return: funkcja zwraca krotke (w,train_err,val_err,regularization_lambda), gdzie w sa parametrami modelu, ktory najlepiej generalizuje dane,
+    tj. daje najmniejszy blad na ciagu walidacyjnym. Wielomian dopasowany jest wg kryterium z regularyzacja. train_err i val_err to
+    bledy na sredniokwadratowe na ciagach treningowym i walidacyjnym. regularization_lambda to najlepsza wartosc parametru regularyzacji
+    '''
+
+    val = []
+    for regularization_lambda in lambda_values:
+        w, _ = regularized_least_squares(x_train, y_train, M, regularization_lambda)
+        err = mean_squared_error(x_val, y_val, w)
+        val.append((w, err, regularization_lambda))
+
+    w, val_err, regularization_lambda = min(val, key=lambda e: e[1])
+
+    train_err = mean_squared_error(x_train, y_train, w)
+
+    return w, train_err, val_err, regularization_lambda
+
